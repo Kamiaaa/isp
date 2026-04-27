311816ef53c8974e310fc4140b21e204ac3a71d5
taeef <kazitaeef@gmail.com>
2026-04-27 20:58:04 +0600

    About page height updated

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -107,7 +107,7 @@ const AboutPage = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 overflow-x-hidden">
+    <div className="min-h-screen mt-20 bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 overflow-x-hidden">
       {/* Hero Section */}
       <motion.div 
         className="relative bg-gray-900 overflow-hidden"
